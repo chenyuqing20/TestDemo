46bbfa2b83e3eda9ddda81d251f9b9db7bc25899
盒子 <hezi@hezidemacbook-air.local>
2018-06-01 15:54:01 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 盒子 on 2018/6/1.
+//  Copyright © 2018年 盒子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
