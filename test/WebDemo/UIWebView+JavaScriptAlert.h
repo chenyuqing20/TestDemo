0698383611f386fef713852916153d45047c1165
盒子 <hezi@hezideMacBook-Air.local>
2018-06-27 19:13:18 +0800

    “添加UIWebview与Js交互，添加Widget小组件，修改JDDAlertView”

---
+++ test/WebDemo/UIWebView+JavaScriptAlert.h
@@ -0,0 +1,13 @@
+//
+//  UIWebView+JavaScriptAlert.h
+//  test
+//
+//  Created by 盒子 on 2018/6/27.
+//  Copyright © 2018年 盒子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWebView (JavaScriptAlert)
+
+@end
---
+++ test/WebDemo/UIWebView+JavaScriptAlert.m
@@ -0,0 +1,13 @@
+//
+//  UIWebView+JavaScriptAlert.m
+//  test
+//
+//  Created by 盒子 on 2018/6/27.
+//  Copyright © 2018年 盒子. All rights reserved.
+//
+
+#import "UIWebView+JavaScriptAlert.h"
+
+@implementation UIWebView (JavaScriptAlert)
+
+@end
